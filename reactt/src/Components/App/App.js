32e8a04ad76f0d5381093197cd151ade26044cce
Miha <lecicmihailo@yahoo.com>
2022-09-12 08:10:37 +0200

    v1.0

--- reactt/src/Components/App/App.js
+++ reactt/src/Components/App/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import React from 'react';
+import Register from '../register/register.js';
 
-function App() {
-  return (
-    <div className="App">
-      Ja imam veliku kuru
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: 'Nabijem te na kurac'
+    };
+    
+    this.kurac = this.kurac.bind(this);
+  }
+  kurac() {
+    this.setState({ data: 'MAJMUNSKA GUZICA!' });
+  }
+  render() {
+    return (
+      <div className='App'>
+        <div className='div'>{this.state.data}</div>
+
+        <Register caller={this.kurac} />
+      </div>
+
+    );
+  }
 }
-
 export default App;
---
+++ reactt/src/Components/register/register.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Register extends React.Component {
+    
+    render() {
+        return (
+            <button onClick={this.props.caller}>TRANSFER</button>
+        );
+    }
+}
+export default Register;
